a261197e063929ab9380ed5396adfb7dfd112f5d
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 13:36:52 +0100

    preparing api

--- src/routes/homeRoutes.ts
+++ src/routes/homeRoutes.ts
@@ -2,6 +2,11 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const homeRouter = Router();
 
-homeRouter.get("/", (req: Request, res: Response, next: NextFunction) => {});
+homeRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({
+    message: "view all set O6 students data",
+  });
+});
 
+export default homeRouter;
 // homeRouter.post()
