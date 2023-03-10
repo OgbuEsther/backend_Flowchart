0b587b4e557bcf7a869a886a46d8824a9ab27d6b
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 12:52:47 +0100

    saved error handler

--- src/middleware/error/index.ts
+++ src/middleware/error/index.ts
@@ -1,6 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError, HttpCode } from "../../utils/appError";
 
+//creating the developer's error
+
+const devErrors = (err: AppError, res: Response) => {
+  return res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+    name: AppError.name,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+    status: err.httpCode,
+  });
+};
+
 //creating our error handler
 export const errorHandler = (
   err: AppError,
