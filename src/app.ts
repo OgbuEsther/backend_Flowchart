7ae741876c99a22c1d2bc988ff4d7e37f5dbbb8f
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 13:57:33 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import cors from "cors";
 import morgan from "morgan";
 import { errorHandler } from "./middleware/error";
 import { AppError, HttpCode } from "./utils/appError";
+import router from "./api";
 
 //creating our application
 const appConfig = (app: Application) => {
   app.use(express.json()).use(cors()).use(morgan("dev"));
+
+  //routes (API endpoints)
+  app.use("/api", router);
 
   //checking all routes
   app.all("*", (req: Request, res: Response, next: NextFunction) => {
