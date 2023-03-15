e0778c54b8736843856a144b4162f3e3c7709246
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-15 14:22:35 +0100

    saved routes

---
+++ src/routes/authroutes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { register } from "../controller/usercontroller";
+import { registerValidation } from "../middleware/validator/userValidation/userValidation";
+
+const userRoutes = Router();
+
+userRoutes.post("/register", registerValidation, register);
