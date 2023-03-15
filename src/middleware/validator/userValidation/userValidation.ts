f968c91a546465d44b7e8e153284bfb23e5a54a5
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-15 13:02:34 +0100

    saved all

--- src/middleware/validator/userValidation/userValidation.ts
+++ src/middleware/validator/userValidation/userValidation.ts
@@ -2,8 +2,10 @@ import { RequestHandler } from "express";
 import { validator } from "../validator";
 import { userSchema } from "./userSchema";
 
-export const registerValidation: RequestHandler = (req, res, next) =>
+export const registerValidation: RequestHandler = (req, res, next) => {
   validator(userSchema.register, req.body, next);
+};
 
-export const loginValidation: RequestHandler = (req, res, next) =>
+export const loginValidation: RequestHandler = (req, res, next) => {
   validator(userSchema.login, req.body, next);
+};
